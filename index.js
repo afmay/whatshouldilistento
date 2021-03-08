3cf5335f5304f7227180eb5480442714a3e1f555
afmay <71290938+afmay@users.noreply.github.com>
2021-03-08 15:27:03 -0700

    added spotify login

---
+++ index.js
@@ -0,0 +1,42 @@
+const app = new Vue({
+    el: '#app',
+    data() {
+      return {
+        client_id: 'b9b1bc33ef1746b0b394e0333f15f036',
+        scopes: 'user-top-read',
+        redirect_uri: 'https://s.codepen.io/leemartin/debug/ffc5867e9dd2a8119bdf9c7735e021cc',
+        me: null
+      }
+    },
+    methods: {
+      login() {
+        let popup = window.open(`https://accounts.spotify.com/authorize?client_id=${this.client_id}&response_type=token&redirect_uri=${this.redirect_uri}&scope=${this.scopes}&show_dialog=true`, 'Login with Spotify', 'width=800,height=600')
+        
+        window.spotifyCallback = (payload) => {
+          // alert(payload)
+          
+          popup.close()
+          
+          fetch('https://api.spotify.com/v1/me', {
+            headers: {
+              'Authorization': `Bearer ${payload}`
+            }
+          }).then(response => {
+            return response.json()
+          }).then(data => {
+            this.me = data
+          })
+        }
+      }
+    },
+    mounted() {
+      this.token = window.location.hash.substr(1).split('&')[0].split("=")[1]
+      
+      if (this.token) {
+        // alert(this.token)
+        
+        window.opener.spotifyCallback(this.token)
+      }
+    }
+  })
+  
